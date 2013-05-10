3bd8a091b5933e2e851f7a0f99989c73c7036f1c
Ted Mielczarek <ted@mielczarek.org>
2013-05-10 15:03:50 -0400

    fix indentation

--- netgame.js
+++ netgame.js
@@ -1,103 +1,103 @@
 "use strict";
 (function(scope) {
-   function callback(thing, method, args) {
-       if(method in thing && 'function' === typeof thing[method]) {
-           thing[method].apply(thing, args);
-       }
-   }
-
-    function netgame() {
-        this.nextSequence = 0;
-        this.lastSeqSent = 0xFFFFFFFF;
-        this.nextAckToSend = 0xFFFFFFFF;
-        this.lastAckReceived = 0xFFFFFFFF;
+  function callback(thing, method, args) {
+    if(method in thing && 'function' === typeof thing[method]) {
+      thing[method].apply(thing, args);
     }
+  }
 
-    netgame.prototype = {
-        // Process a packet from an ArrayBuffer.
-        processPacket: function(buf) {
-            var bufview = new DataView(buf);
-            //TODO: sanity check seq here
-            var seq = bufview.getUint32(0, true);
-            var ack = bufview.getUint32(4, true);
-            this.nextAckToSend = seq;
-            if (ack != 0xFFFFFFFF &&
-                ack <= this.lastSeqSent &&
-                (ack > this.lastAckReceived ||
-                 this.lastAckReceived == 0xFFFFFFFF)) {
-                this.lastAckReceived = ack;
-                callback(this, "onack", [ack]);
-            }
+  function netgame() {
+    this.nextSequence = 0;
+    this.lastSeqSent = 0xFFFFFFFF;
+    this.nextAckToSend = 0xFFFFFFFF;
+    this.lastAckReceived = 0xFFFFFFFF;
+  }
 
-            return true;
-        },
+  netgame.prototype = {
+    // Process a packet from an ArrayBuffer.
+    processPacket: function(buf) {
+      var bufview = new DataView(buf);
+      //TODO: sanity check seq here
+      var seq = bufview.getUint32(0, true);
+      var ack = bufview.getUint32(4, true);
+      this.nextAckToSend = seq;
+      if (ack != 0xFFFFFFFF &&
+          ack <= this.lastSeqSent &&
+          (ack > this.lastAckReceived ||
+           this.lastAckReceived == 0xFFFFFFFF)) {
+        this.lastAckReceived = ack;
+        callback(this, "onack", [ack]);
+      }
 
-        // Construct a packet and send it by calling
-        // sender, passing it the ArrayBuffer of the
-        // packet's contents. data is the packet payload as an ArrayBuffer.
-        sendPacket: function(sender, data) {
-            var HEADER_SIZE = 8;
-            var seq = this.nextSequence;
-            this.lastSeqSent = seq;
-            this.nextSequence++;
-            var size = HEADER_SIZE;
-            if (data)
-                size += data.byteLength;
-            var buf = new ArrayBuffer(size);
-            var bufview = new DataView(buf);
-            bufview.setUint32(0, seq, true);
-            bufview.setUint32(4, this.nextAckToSend, true);
-            if (data) {
-                var u8buf = new Uint8Array(buf);
-                u8buf.set(data, HEADER_SIZE);
-            }
-            sender(buf);
-        }
-    };
+      return true;
+    },
 
-    function netprop(type, default_value) {
-        this.type = type;
-        this.value = default_value || 0;
+    // Construct a packet and send it by calling
+    // sender, passing it the ArrayBuffer of the
+    // packet's contents. data is the packet payload as an ArrayBuffer.
+    sendPacket: function(sender, data) {
+      var HEADER_SIZE = 8;
+      var seq = this.nextSequence;
+      this.lastSeqSent = seq;
+      this.nextSequence++;
+      var size = HEADER_SIZE;
+      if (data)
+        size += data.byteLength;
+      var buf = new ArrayBuffer(size);
+      var bufview = new DataView(buf);
+      bufview.setUint32(0, seq, true);
+      bufview.setUint32(4, this.nextAckToSend, true);
+      if (data) {
+        var u8buf = new Uint8Array(buf);
+        u8buf.set(data, HEADER_SIZE);
+      }
+      sender(buf);
     }
+  };
 
-     function read_u32(dataview, offset) {
-         this.value = dataview.getUint32(offset, true);
-     }
-     function write_u32(dataview, offset) {
-         dataview.setUint32(offset, this.value, true);
-     }
-     function read_i32(dataview, offset) {
-         this.value = dataview.getInt32(offset, true);
-     }
-     function write_i32(dataview, offset) {
-         dataview.setInt32(offset, this.value, true);
-     }
+  function netprop(type, default_value) {
+    this.type = type;
+    this.value = default_value || 0;
+  }
 
-     // Default types
-     netprop.u32 = {size: 4, read: read_u32, write: write_u32 };
-     netprop.i32 = {size: 4, read: read_i32, write: write_i32 };
+  function read_u32(dataview, offset) {
+    this.value = dataview.getUint32(offset, true);
+  }
+  function write_u32(dataview, offset) {
+    dataview.setUint32(offset, this.value, true);
+  }
+  function read_i32(dataview, offset) {
+    this.value = dataview.getInt32(offset, true);
+  }
+  function write_i32(dataview, offset) {
+    dataview.setInt32(offset, this.value, true);
+  }
 
-     netprop.prototype = {
-         read: function(dataview, offset) {
-             this.type.read.apply(this, [dataview, offset]);
-             return offset + this.type.size;
-         },
-         write: function(dataview, offset) {
-             this.type.write.apply(this, [dataview, offset]);
-             return offset + this.type.size;
-         }
-     };
+  // Default types
+  netprop.u32 = {size: 4, read: read_u32, write: write_u32 };
+  netprop.i32 = {size: 4, read: read_i32, write: write_i32 };
 
-    function client_net() {
-        //TODO
+  netprop.prototype = {
+    read: function(dataview, offset) {
+      this.type.read.apply(this, [dataview, offset]);
+      return offset + this.type.size;
+    },
+    write: function(dataview, offset) {
+      this.type.write.apply(this, [dataview, offset]);
+      return offset + this.type.size;
     }
+  };
 
-    function server_net() {
-        //TODO
-    }
+  function client_net() {
+    //TODO
+  }
 
-    scope.netgame = netgame;
-    scope.netprop = netprop;
-    scope.client_net = client_net;
-    scope.server_net = server_net;
+  function server_net() {
+    //TODO
+  }
+
+  scope.netgame = netgame;
+  scope.netprop = netprop;
+  scope.client_net = client_net;
+  scope.server_net = server_net;
 })(window);
--- test.js
+++ test.js
@@ -1,38 +1,38 @@
 test("Send", function() {
-    var n = new netgame();
-    n.sendPacket(function(buf) {
-      equals(buf.byteLength, 8);
-      var u32 = new Uint32Array(buf);
-      equals(u32[0], 0);
-      equals(u32[01], 0xFFFFFFFF);  // nothing to ack
-    });
-    var lastAck = null;
-    n.onack = function(ack) {
-         lastAck = ack;
-    };
-    n.sendPacket(function(buf) {
-      equals(buf.byteLength, 8);
-      var u32 = new Uint32Array(buf);
-      equals(u32[0], 1);
-      equals(u32[01], 0xFFFFFFFF);  // nothing to ack
-    });
-    equals(lastAck, null);
-    // Now pass in a packet.
-    var pkt = new Uint32Array(2);
-    pkt[0] = 0xABABCDCD;
-    pkt[1] = 0xFFFFFFFF;
-    equals(n.processPacket(pkt.buffer), true);
-    n.sendPacket(function(buf) {
-      equals(buf.byteLength, 8);
-      var u32 = new Uint32Array(buf);
-      equals(u32[0], 2);
-      equals(u32[01], 0xABABCDCD);  // acking previously received packet
-    });
-    pkt[0] = 0xABABCDCE;
-    // pretend to actually ack a packet
-    pkt[1] = 0;
-    equals(n.processPacket(pkt.buffer), true);
-    equals(lastAck, 0);
+  var n = new netgame();
+  n.sendPacket(function(buf) {
+    equals(buf.byteLength, 8);
+    var u32 = new Uint32Array(buf);
+    equals(u32[0], 0);
+    equals(u32[01], 0xFFFFFFFF);  // nothing to ack
+  });
+  var lastAck = null;
+  n.onack = function(ack) {
+    lastAck = ack;
+  };
+  n.sendPacket(function(buf) {
+    equals(buf.byteLength, 8);
+    var u32 = new Uint32Array(buf);
+    equals(u32[0], 1);
+    equals(u32[01], 0xFFFFFFFF);  // nothing to ack
+  });
+  equals(lastAck, null);
+  // Now pass in a packet.
+  var pkt = new Uint32Array(2);
+  pkt[0] = 0xABABCDCD;
+  pkt[1] = 0xFFFFFFFF;
+  equals(n.processPacket(pkt.buffer), true);
+  n.sendPacket(function(buf) {
+    equals(buf.byteLength, 8);
+    var u32 = new Uint32Array(buf);
+    equals(u32[0], 2);
+    equals(u32[01], 0xABABCDCD);  // acking previously received packet
+  });
+  pkt[0] = 0xABABCDCE;
+  // pretend to actually ack a packet
+  pkt[1] = 0;
+  equals(n.processPacket(pkt.buffer), true);
+  equals(lastAck, 0);
 });
 
 test("netprop.u32", function() {
