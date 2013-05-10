e2019e2b71cc1e97b2ffc3ab53941ebb7f426f16
Ted Mielczarek <ted@mielczarek.org>
2013-05-10 13:47:01 -0400

    Remove BufferView, tweak some things

--- netgame.js
+++ netgame.js
@@ -1,5 +1,11 @@
 "use strict";
 (function(scope) {
+   function callback(thing, method, args) {
+       if(method in thing && 'function' === typeof thing[method]) {
+           thing[method].apply(thing, args);
+       }
+   }
+
     function netgame() {
         this.nextSequence = 0;
         this.lastSeqSent = 0xFFFFFFFF;
@@ -10,14 +16,14 @@
     netgame.prototype = {
         // Process a packet from an ArrayBuffer.
         processPacket: function(buf) {
-            var bufview = new BufferView(buf, BufferView.LE);
-            var seq = bufview.readUnsignedInt();
-            var ack = bufview.readUnsignedInt();
+            var bufview = new DataView(buf);
+            var seq = bufview.getUint32(0, true);
+            var ack = bufview.getUint32(4, true);
             this.nextAckToSend = seq;
             if (ack != 0xFFFFFFFF &&
                 ack <= this.lastSeqSent &&
                 ack > this.lastAckReceived) {
-                this.lastAckReceived = ack;                
+                this.lastAckReceived = ack;
             }
 
             return true;
@@ -25,19 +31,36 @@
 
         // Construct a packet and send it by calling
         // sender, passing it the ArrayBuffer of the
-        // packet's contents.
-        sendPacket: function(sender) {
+        // packet's contents. data is the packet payload as an ArrayBuffer.
+        sendPacket: function(sender, data) {
+            var HEADER_SIZE = 8;
             var seq = this.nextSequence;
             this.lastSeqSent = seq;
             this.nextSequence++;
-            var buf = new ArrayBuffer(8); //XXX: this sucks
-            var bufview = new BufferView(buf, BufferView.LE);
-            bufview.writeUnsignedInt(seq);
-            bufview.writeUnsignedInt(this.nextAckToSend);
-            //TODO: actually write data
+            var size = HEADER_SIZE;
+            if (data)
+                size += data.byteLength;
+            var buf = new ArrayBuffer(size);
+            var bufview = new DataView(buf);
+            bufview.setUint32(0, seq, true);
+            bufview.setUint32(4, this.nextAckToSend, true);
+            if (data) {
+                var u8buf = new Uint8Array(buf);
+                u8buf.set(data, HEADER_SIZE);
+            }
             sender(buf);
         }
     };
 
+    function client_net() {
+        //TODO
+    }
+
+    function server_net() {
+        //TODO
+    }
+
     scope.netgame = netgame;
+    scope.client_net = client_net;
+    scope.server_net = server_net;
 })(window);
