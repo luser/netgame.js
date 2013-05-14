b88f3542e40c13a407d8bba534830b7cc74ac848
Ted Mielczarek <ted@mielczarek.org>
2013-05-14 09:29:09 -0400

    update from master

--- examples/local.js
+++ examples/local.js
@@ -20,13 +20,31 @@ ball.prototype.draw = function(cx) {
   cx.fill();
 };
 
+function block() {
+  netobject.call(this, {x: netprop.u8, y: netprop.u8});
+}
+
+block.prototype = netobject.register(block);
+
+block.prototype.draw = function(cx) {
+  cx.fillStyle = "red";
+  cx.fillRect(this.x - 2, this.y - 2, 4, 4);
+};
+
 var WIDTH = 200, HEIGHT = 200;
-var balls = [];
+var things = [];
 var server = new server_net();
 var client = new client_net({send: function(data) {}}); // Client doesn't currently send any data.
-server.addClient(new server_client({send: function(data) { client.recv(data); }}));
+function client_recv(data) {
+  if (Math.random() > packetLoss) {
+    client.recv(data);
+  }
+}
+
+server.addClient(new server_client({send: client_recv}));
 var intervalID = null;
 var lastUpdate = performance.now();
+var packetLoss = 0;
 
 function randX() {
    return Math.floor(Math.random() * (WIDTH + 1));
@@ -43,30 +61,30 @@ function randDir() {
 }
 
 function setup() {
-  var numBalls = Math.floor(Math.random() * 10) + 1;
-  console.log("numBalls: %d", numBalls);
-  for (var i = 0; i < numBalls; i++) {
-    var b = new ball();
+  var numThings = 10;
+  for (var i = 0; i < numThings; i++) {
+    var b = Math.random() < 0.5 ? new ball() : new block();
     b.x = randX();
     b.y = randY();
     b.dir = randDir();
-    balls.push(b);
+    things.push(b);
   }
   lastUpdate = performance.now();
   intervalID = setInterval(runServerFrame, 100);
   requestAnimationFrame(redraw);
+  document.getElementById("packetloss").value = 0;
 }
 
 function runServerFrame() {
   var now = performance.now();
   // Move all things.
   //TODO: make movement time-relative
-  for (var i = 0; i < balls.length; i++) {
-    var b = balls[i];
-    b.x = (b.x + b.dir.x) % WIDTH;
-    b.y = (b.y + b.dir.y) % HEIGHT;
+  for (var i = 0; i < things.length; i++) {
+    var t = things[i];
+    t.x = (t.x + t.dir.x) % WIDTH;
+    t.y = (t.y + t.dir.y) % HEIGHT;
   }
-  server.updateClients(balls);
+  server.updateClients(things);
   lastUpdate = now;
 }
 
@@ -78,9 +96,14 @@ function drawWorld(cx, things) {
 }
 
 function redraw() {
-  drawWorld(document.getElementById("server").getContext("2d"), balls);
+  drawWorld(document.getElementById("server").getContext("2d"), things);
   drawWorld(document.getElementById("client").getContext("2d"), client.things);
   requestAnimationFrame(redraw);
+}
+
+function updatePacketLoss(value) {
+  document.getElementById("packetloss-value").firstChild.textContent = value;
+  packetLoss = value / 100.0;
 }
 
 addEventListener("load", setup);
