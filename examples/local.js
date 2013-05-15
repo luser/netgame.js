bee94c4e606c4e3a91fc4e3b6eb78ce2589c8420
Ted Mielczarek <ted@mielczarek.org>
2013-05-15 19:15:25 -0400

    update from master

--- examples/local.js
+++ examples/local.js
@@ -2,7 +2,15 @@
   var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                               window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
   window.requestAnimationFrame = requestAnimationFrame;
+
+  if (!('performance' in window)) {
+    window.performance = {};
+  }
+  if (!('now' in window.performance)) {
+    window.performance.now = Date.now;
+  }
 })();
+
 
 // Really simple object, just 1-byte X and Y coordinates.
 function ball() {
@@ -32,19 +40,46 @@ block.prototype.draw = function(cx) {
 };
 
 var WIDTH = 200, HEIGHT = 200;
+var server_rate = 15;
+var transmit_rate = 50;
 var things = [];
 var server = new server_net();
 var client = new client_net({send: function(data) {}}); // Client doesn't currently send any data.
+server.addClient(new server_client({send: client_recv}));
+
+var serverIntervalID = null;
+var transmitIntervalID = null;
+var lastUpdate = performance.now();
+var packetLoss = 0;
+var latency = 0;
+var CLIENT_PACKET_SAMPLES = 20;
+var clientPackets = [];
+var lastRateUpdate = 0;
+
 function client_recv(data) {
+  updateClientDataRate(data.byteLength);
   if (Math.random() > packetLoss) {
-    client.recv(data);
+    setTimeout(function() { client.recv(data); }, latency);
   }
 }
 
-server.addClient(new server_client({send: client_recv}));
-var intervalID = null;
-//var lastUpdate = performance.now();
-var packetLoss = 0;
+function updateClientDataRate(bytes) {
+  var now = performance.now();
+  clientPackets.push([now, bytes]);
+  if (clientPackets.length > CLIENT_PACKET_SAMPLES) {
+    clientPackets.shift();
+  }
+  if (clientPackets.length == CLIENT_PACKET_SAMPLES && (now - lastRateUpdate) > 1000) {
+    lastRateUpdate = now;
+    var elapsed = (now - clientPackets[0][0]) / 1000;
+    var total = 0;
+    for (var i = 0; i < clientPackets.length; i++) {
+      total += clientPackets[i][1];
+    }
+    var rate = ((total * 8) / 1024) / elapsed;
+    document.getElementById("client-rate").firstChild.textContent = Math.round(rate * 100) / 100;
+  }
+}
 
 function randX() {
    return Math.floor(Math.random() * (WIDTH + 1));
@@ -56,8 +91,8 @@ function randY() {
 
 function randDir() {
   //TODO: support negative directions
-  return {x: Math.floor(Math.random() * (5 + 1)),
-          y: Math.floor(Math.random() * (5 + 1))};
+  return {x: Math.floor(Math.random() * (20 + 1)),
+          y: Math.floor(Math.random() * (20 + 1))};
 }
 
 function setup() {
@@ -69,23 +104,27 @@ function setup() {
     b.dir = randDir();
     things.push(b);
   }
-  //lastUpdate = performance.now();
-  intervalID = setInterval(runServerFrame, 100);
+  lastUpdate = performance.now();
+  serverIntervalID = setInterval(runServerFrame, server_rate);
+  transmitIntervalID = setInterval(sendToClients, transmit_rate);
   requestAnimationFrame(redraw);
   document.getElementById("packetloss").value = 0;
 }
 
 function runServerFrame() {
-  //var now = performance.now();
+  var now = performance.now();
+  var elapseds = (now - lastUpdate) / 1000.0;
   // Move all things.
-  //TODO: make movement time-relative
   for (var i = 0; i < things.length; i++) {
     var t = things[i];
-    t.x = (t.x + t.dir.x) % WIDTH;
-    t.y = (t.y + t.dir.y) % HEIGHT;
+    t.x = (t.x + t.dir.x*elapseds) % WIDTH;
+    t.y = (t.y + t.dir.y*elapseds) % HEIGHT;
   }
+  lastUpdate = now;
+}
+
+function sendToClients() {
   server.updateClients(things);
-  //lastUpdate = now;
 }
 
 function drawWorld(cx, things) {
@@ -99,6 +138,11 @@ function redraw() {
   drawWorld(document.getElementById("server").getContext("2d"), things);
   drawWorld(document.getElementById("client").getContext("2d"), client.things);
   requestAnimationFrame(redraw);
+}
+
+function updateLatency(value) {
+  document.getElementById("latency-value").firstChild.textContent = value;
+  latency = value;
 }
 
 function updatePacketLoss(value) {
